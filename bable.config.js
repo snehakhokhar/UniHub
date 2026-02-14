4fc3b931db0d8f5c0b067a064452c5faa0908339
savitri <savitrigamit123456@gmail.com>
2026-02-14 21:13:10 +0530

    added cloudinary, image upload

--- bable.config.js
+++ bable.config.js
@@ -4,6 +4,6 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     // plugins: [
     //   require.resolve("expo-router/babel"), // Required for expo-router to work properly
-    // ],
+   // ],
   };
 };
